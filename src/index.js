638b06048638fcbde1e1ebd2215c238e4ee37aaf
Pedash <pedashhelena@gmail.com>
2020-03-17 14:22:56 +0200

    Feat: typical array problem is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array !== undefined && array.length) {
+        return Math.min(...array);
+    }
+    return 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array !== undefined && array.length) {
+        return Math.max(...array)
+    }
+    return 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array !== undefined && array.length) {
+        let amount = array.length;
+        return (array.reduce((a, b) => a + b, 0)) / amount
+    }
+    return 0
 }
